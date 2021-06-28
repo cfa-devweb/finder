e807e2c768769e0d3b79d9fee353052b2f2a1507
Kendrick Francine <k.francine@cci-formation.nc>
2021-06-28 14:29:49 +1100

    add bootstrap dependency

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- `<link href="{{ asset('css/app.css') }}" rel="stylesheet">` -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
