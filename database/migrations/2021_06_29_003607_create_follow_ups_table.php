e179303fc6b9e10594f29889897a571379236c4a
Kendrick Francine <k.francine@cci-formation.nc>
2021-06-29 14:15:41 +1100

    database migration v1

---
+++ database/migrations/2021_06_29_003607_create_follow_ups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowUpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow_ups', function (Blueprint $table) {
+            $table->id();
+            $table->string('date');            
+            $table->string('comment');
+            $table->string('mode_contact');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow_ups');
+    }
+}
