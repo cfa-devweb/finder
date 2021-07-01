6348417890d6ded19f4818e0378cd06e94a2a14d
Jean Masso <68371410+jeanmasso@users.noreply.github.com>
2021-07-02 08:25:28 +1100

    save branch jean

---
+++ resources/views/tableau-suivi/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer=""></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+</head>
+<body class="antialiased">
+
+    <div class="container">
+        <button type="button" class="btn btn-primary">Relance</button>
+    </div>
+
+</body>
+</html>
