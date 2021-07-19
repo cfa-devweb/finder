3480ba7320380dcd15bfd833dbe22a25836476c2
Jean Masso <68371410+jeanmasso@users.noreply.github.com>
2021-07-19 15:31:11 +1100

    Création de modal pour l'ajout de prise de contact

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer=""></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -12,9 +11,10 @@
 
     <main class="container">
     @yield('content')
-    </main>    
-    <footer>   
+    </main>
+    <footer>
 
     </footer>
+    <script src="{{ asset('js/app.js') }}" defer=""></script>
     </body>
 </html>
