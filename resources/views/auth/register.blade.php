81e6a077bb4726c2bfcae08fb177f39552ef34a1
Kendrick Francine <k.francine@cci-formation.nc>
2021-07-19 14:25:46 +1100

    add migration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@include('partials.navbar')
 
 @section('content')
 <div class="container">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,13 +19,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __("S'enregistrer") }}</a>
                                 </li>
                             @endif
                         @else
