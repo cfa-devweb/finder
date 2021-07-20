2a58eeecc2bee30f37c798a3f874bc8def3c12be
unknown <b.desgrippes@cci-formation.nc>
2021-07-20 18:42:06 +1100

    WIP READ data

--- resources/views/listingPosts.blade.php
+++ resources/views/listingPosts.blade.php
@@ -15,72 +15,6 @@
 
         
 
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"> Nouvelle offre d'alternance</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="First name"
-                                    aria-label="First name">
-                            </div>
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="First name"
-                                    aria-label="First name">
-                            </div>
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div>
-        <table class="table table-striped my-3">
-            <thead class="table-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Intitulé</th>
-                    <th scope="col">Entreprises</th>
-                    <th scope="col">Domaine</th>
-                    <th scope="col">Contacts</th>
-                    <th scope="col">Description de l'offre</th>
-                    <th class="text-center" scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-            {{-- @foreach ($listingPost as $listingPost) --}}
-                <tr>
-                    <th scope="row">1</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="d-flex justify-content-evenly">
-                        <button type="button" class="btn btn-warning btn-sm">M</button>
-                        <button type="button" class="btn btn-danger btn-sm">X</button>
-                    </td>
-                </tr>
-            {{-- @endforeach --}}
-            </tbody>
-        </table>
-
         <!-- Modal add offer -->
         <form action="" method="POST">
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
@@ -128,5 +62,39 @@
                 </div>
             </div>
         </form>
+    </div>
+
+    <div>
+        <table class="table table-striped my-3">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Intitulé</th>
+                    <th scope="col">Entreprises</th>
+                    <th scope="col">Domaine</th>
+                    <th scope="col">Contacts</th>
+                    <th scope="col">Description de l'offre</th>
+                    <th class="text-center" scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+             @foreach ($Posts as $key)
+                <tr>
+                    <th scope="row">{{$key->id}}</th>
+                    <td>{{$key->name}}</td>
+                    <td>{{$key->company_name}}</td>
+                    <td>{{$key->domaine}}</td>
+                    <td>{{$key->contact}}</td>
+                    <td>{{$key->content}}</td>
+                    <td class="d-flex justify-content-evenly">
+                        <button type="button" class="btn btn-warning btn-sm">M</button>
+                        <button type="button" class="btn btn-danger btn-sm">X</button>
+                    </td>
+                </tr>
+             @endforeach 
+            </tbody>
+        </table>
+
+        
     </div>
     @endsection
